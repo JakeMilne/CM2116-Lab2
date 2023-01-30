cb50f4dec4b0b7a802504908f2c4381a9c79dd8a
Jake <j.milne27@rgu.ac.uk>
2023-01-30 16:25:55 +0000

    lab2

--- src/PowerExperiments.java
+++ src/PowerExperiments.java
@@ -1,19 +1,49 @@
 public class PowerExperiments {
+    static int count = 0;
     public static void main(String[] args) throws Exception {
         simplePowerExperiments();
+        
     }
 
     static double power(double x, int n){
         double p = 1;
-
+        count++;
         for(int i = 1; i <= n; i++){
             p *= x;
+            count++;
         }
 
         return p;
+        //O(n)
     }
-
+    static int smartPower(double x, int n){
+        int p=1;
+        count++;
+        double q=x;
+        count++;
+        int m=n;
+        count++;
+        while(m>0){
+            if(m%2 == 1){
+                p = (int)(p*q);
+                count++;
+            }
+            m = (int)(Math.floor(m/2));
+            count++;
+            q = q*q;
+            count++;
+        }
+        //O(logn)  
+        return (p);
+    }
     static void simplePowerExperiments(){
-        
+        System.out.println("n,count,ops/logn");
+        for(int i=0;i<20;i++){
+            count = 0;
+            smartPower(2, i);
+            float logn = (int)(100*count/Math.log(i));   
+            logn = logn/100; //2dp
+            System.out.println(i + "," + count + "," + logn);
+        }
     }
 }
